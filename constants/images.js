d25e11d09cdd36973434aa3566c7ac1140601599
ArchieSachin <archieshah8767@gmail.com>
2024-10-24 23:09:43 +0530

    clearing previous files

--- constants/images.js
+++
@@ -1,9 +0,0 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
-
-import path from "../assets/images/path.png";
-import logo from "../assets/images/MindMend.png";
-import empty from "../assets/images/empty.png";
-import hero from "../assets/images/hero-bg.png"
-
-export default { profile, thumbnail, path, logo,  empty , hero};
--- context/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import myReducer from "./reducers";
-
-const Store = createStore(myReducer);
-
-export default Store;
